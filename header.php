e75d8660df38958f8fb1c30848d35d937112de7b
Julien Chaumond <chaumond@gmail.com>
2012-05-16 16:04:40 +0200

    Fix YouTube JS Api inclusion bug (HTTPS)

--- header.php
+++ header.php
@@ -132,7 +132,7 @@ window.twttr.ready(function(){
 
 <script type="text/javascript">
   var tag = document.createElement('script');
-  tag.src = "http://www.youtube.com/player_api";
+  tag.src = "//www.youtube.com/player_api";
   var firstScriptTag = document.getElementsByTagName('script')[0];
   firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 </script>
