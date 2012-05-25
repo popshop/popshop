d7fae975bcf4aa357f33ebd3deb8a5c0a36ba8ce
Paul Marsden <paul.marsden@mac.com>
2012-05-25 13:24:29 +0100

    Updated Copy

--- admin/settings.gettingstarted.php
+++ admin/settings.gettingstarted.php
@@ -1,18 +1,18 @@
 
 <div class="slide" id="slide-shop_type">
     
-    <h2>Hello, what kind of Popshop event would you like to setup?</h2>
+    <h2>Hello, what kind of pop-up event would you like to run?</h2>
     
     <div class="startbox cf" data-shop="sell">
         <div class="startbox-img sell"></div>
         <div class="startbox-text">Social Sampling</div>
-        <div class="startbox-legend">Offer a free sample or download in return for a social share or contact share</div>
+        <div class="startbox-legend">Offer free stuff for a social share, contact share or just for free</div>
     </div>
     
     <div class="startbox cf" data-shop="link">
         <div class="startbox-img link"></div>
-        <div class="startbox-text">Product Showcase</div>
-        <div class="startbox-legend">Showcase a new product on and redirect people to a chosen URL</div>
+        <div class="startbox-text">Social Showcase</div>
+        <div class="startbox-legend">Showcase something new and redirect people to a chosen URL</div>
     </div>
     
 </div>
@@ -20,7 +20,7 @@
 
 <div class="slide" id="slide-payment_type" style="display:none;">
     
-    <h2>What type of payment do you want to set up?</h2>
+    <h2>What type of 'payment' do you want to set up?</h2>
     
     <div class="startbox cf" data-payment="free">
         <div class="startbox-img free"></div>
@@ -31,19 +31,19 @@
     <div class="startbox cf" data-payment="share">
         <div class="startbox-img share"></div>
         <div class="startbox-text">Free for a Share</div>
-        <div class="startbox-legend">Customer Pays with Like, Tweet, Pin, +1 or LinkedIn Share</div>
+        <div class="startbox-legend">Guest pays with a Like, Tweet, Pin, +1 or LinkedIn Share</div>
     </div>
     
     <div class="startbox cf" data-payment="contact">
         <div class="startbox-img contact"></div>
         <div class="startbox-text">Free for a Contact</div>
-        <div class="startbox-legend">Customer pays with their contact details</div>
+        <div class="startbox-legend">Guest pays with their contact details</div>
     </div>
     
     <div class="startbox cf" data-payment="paypal" style="display:none;">
         <div class="startbox-img cash"></div>
         <div class="startbox-text">Pay with PayPal</div>
-        <div class="startbox-legend">Customer pays via PayPal (PayPal account or payment card)</div>
+        <div class="startbox-legend">Guest pays via PayPal (PayPal account or payment card)</div>
     </div>
     
     <a href="#" class="slide-back" data-slide="#slide-shop_type">Back</a>
@@ -52,7 +52,7 @@
 
 <div class="slide" id="slide-product_type" style="display:none;">
     
-    <h2>What kind of product would you like to sample?</h2>
+    <h2>What kind of sample will you be offering?</h2>
     
     <div class="startbox cf" data-product="file">
         <div class="startbox-img file"></div>
